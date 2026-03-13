cc6570de540e82b8ae591c5b0924d337e5ae5769
UErden <utku.erden@fugo.com.tr>
2026-03-13 23:18:47 +0300

    Button fix

--- Assets/_Project/Tests/Scripts/TestProjectInstaller.cs
+++ Assets/_Project/Tests/Scripts/TestProjectInstaller.cs
@@ -1,12 +1,14 @@
 using Simon.Core.DI;
 using Simon.Core.Events;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Simon.Tests
 {
     internal class TestProjectInstaller : MonoInstaller
     {
         [SerializeField] private Camera _mainCamera;
+        [SerializeField] private EventSystem _eventSystem;
 
         public override void InstallBindings()
         {
@@ -16,6 +18,11 @@ namespace Simon.Tests
             {
                 _mainCamera.transform.SetParent(transform);
                 Container.Bind<Camera>().FromInstance(_mainCamera).AsSingle();
+            }
+            if (_eventSystem != null)
+            {
+                _eventSystem.transform.SetParent(transform);
+                Container.Bind<EventSystem>().FromInstance(_eventSystem).AsSingle();
             }
 
             Container.Bind<TestSceneManager>().AsSingle();
