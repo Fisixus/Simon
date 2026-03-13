5bcc78bd250ca7a821dc9598e96f841499857360
UErden <utku.erden@fugo.com.tr>
2026-03-13 21:11:09 +0300

    SceneManager bug fix

--- Assets/_Project/Tests/Scripts/TestSceneManager.cs
+++ Assets/_Project/Tests/Scripts/TestSceneManager.cs
@@ -1,16 +1,18 @@
 using Simon.Core.DI;
 using Simon.Core.Events;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Simon.Tests
 {
-    public class TestSceneManager
+     public class TestSceneManager
     {
         [Inject] private SignalBus _signalBus;
 
         public void LoadMainMenu()
         {
-            Debug.Log("[TestSceneManager] Loading Main Menu...");
+            SceneManager.LoadScene("TestMainMenu");
+            Debug.Log("[TestSceneManager] Invoking TestLoadMainMenuSignal...");
             _signalBus.Invoke(new TestLoadMainMenuSignal());
         }
     }
